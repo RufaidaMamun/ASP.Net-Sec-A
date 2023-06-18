d32f0d5d5ab048f0d9a8d743043208e5025b151d
RufaidaMamun <rufaidamamun@gmail.com>
2023-06-19 00:21:26 +0600

    Form Validation

---
+++ StudentForm/StudentForm/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using StudentForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            //ViewBag.Name = Request["Username"];
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string Username, string Password)
+        {
+            //ViewBag.Name = form["Username"];
+            ViewBag.Name = Username;
+            return View();
+        }
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(Student u)
+        {
+
+            var test = u.Dob.Date.ToString("yyyy-MM-dd");
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Login");
+            }
+            return View(u);
+        }
+    }
+}
---
+++ StudentForm/StudentForm/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StudentForm.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "Please provide your name")]
+        public string Name { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Profession { get; set; }
+        [Required]
+        public DateTime Dob { get; set; }
+
+        public string StudentID { get; set; }
+
+        public string StudentEmail { get; set; }
+    }
+}
